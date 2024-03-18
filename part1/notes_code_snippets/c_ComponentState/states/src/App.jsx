0d2722c202089291cd7f80eae4da4601f06374e8
Angel Motta <angel.motta@utec.edu.pe>
2024-03-18 13:10:42 -0500

    feat: component states

---
+++ part1/notes_code_snippets/c_ComponentState/states/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const App = () => {
+    const [counter, setCounter] = useState(0);
+    console.log("rendering with counter value", counter);
+
+    const increaseByOne = () => {
+        console.log("increasing, value before", counter);
+        setCounter(counter + 1);
+    };
+
+    const decreaseByOne = () => {
+        console.log("decreasing, value before", counter);
+        setCounter(counter - 1);
+    };
+    const setToZero = () => {
+        console.log("resetting to zero, value before", counter);
+        setCounter(0);
+    };
+
+    return (
+        <div>
+            <Display counter={counter} />
+            <Button onClick={increaseByOne} text="Plus" />
+            <Button onClick={setToZero} text="Reset" />
+            <Button onClick={decreaseByOne} text="Minus" />
+        </div>
+    );
+};
+
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+export default App;
