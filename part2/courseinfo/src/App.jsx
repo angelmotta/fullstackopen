080e5dad6bcc93ca397affcecfe6594abe1a9d0c
Angel Motta <angel.motta@utec.edu.pe>
2024-04-25 19:18:00 -0500

    feat: courseinfo step 6

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = ({ course }) => <h1>{course}</h1>;
 
-function App() {
-  const [count, setCount] = useState(0)
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
 
-  return (
+const Part = ({ part }) => (
+    <p>
+        {part.name} {part.exercises}
+    </p>
+);
+
+const Content = ({ parts }) => (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        {parts.map((partObj) => (
+            <Part key={partObj.id} part={partObj} />
+        ))}
     </>
-  )
-}
+);
 
-export default App
+const Course = ({ course }) => {
+    const { name, parts } = course;
+    const totalExercises = parts.reduce(
+        (accum, partObj) => accum + partObj.exercises,
+        0
+    );
+
+    return (
+        <div>
+            <Header course={name} />
+            <Content parts={parts} />
+            <Total sum={totalExercises} />
+        </div>
+    );
+};
+
+const App = () => {
+    const course = {
+        id: 1,
+        name: "Half Stack application development",
+        parts: [
+            {
+                name: "Fundamentals of React",
+                exercises: 10,
+                id: 1,
+            },
+            {
+                name: "Using props to pass data",
+                exercises: 7,
+                id: 2,
+            },
+            {
+                name: "State of a component",
+                exercises: 14,
+                id: 3,
+            },
+        ],
+    };
+
+    return <Course course={course} />;
+};
+
+export default App;
