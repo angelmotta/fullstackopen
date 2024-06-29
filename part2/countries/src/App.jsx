a75190e3a0c23911001dbff3f64d89ff14241578
Angel Motta <angel.motta@utec.edu.pe>
2024-06-28 20:24:00 -0500

    feat: countries app init

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+    return (
+        <div>
+            <h1>Countries App</h1>
+            <div>
+                find countries
+                <input></input>
+            </div>
+        </div>
+    );
+};
+
+export default App;
