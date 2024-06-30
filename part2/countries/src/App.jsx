92e2fbd2f45c20e070339b64141205b581cab6ed
Angel Motta <angel.motta@utec.edu.pe>
2024-06-30 18:55:25 -0500

    feat: handle input

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,11 +1,29 @@
+import { useState, useEffect } from "react";
+
+const Filter = ({ searchCountry, handleOnChange }) => {
+    return (
+        <div>
+            find countries
+            <input value={searchCountry} onChange={handleOnChange}></input>
+        </div>
+    );
+};
+
 const App = () => {
+    const [country, setCountry] = useState("");
+
+    const onChangeCountry = (e) => {
+        let country = e.target.value;
+        country = country.toLowerCase();
+        setCountry(country);
+        console.log(`search input:`);
+        console.log(country);
+    };
+
     return (
         <div>
             <h1>Countries App</h1>
-            <div>
-                find countries
-                <input></input>
-            </div>
+            <Filter searchCountry={country} handleOnChange={onChangeCountry} />
         </div>
     );
 };
